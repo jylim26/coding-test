ca2f6ae9f0da7387cf060154663aa03fa65f90ef
jylim26 <yejong98@gmail.com>
2025-09-02 16:18:50 +0900

    problem_14425 문자열 집합

---
+++ src/backjun/problem_14425.java
@@ -0,0 +1,27 @@
+package backjun;
+
+import java.io.*;
+import java.util.*;
+
+public class problem_14425 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		Set<String> set = new HashSet<>();
+		for (int i=0; i<N; i++) {
+			String line = br.readLine();
+			set.add(line);
+		}
+
+		int count = 0;
+		for (int i=0; i<M; i++) {
+			if (set.contains(br.readLine())) count++;
+		}
+
+		System.out.println(count);
+	}
+}
