39d07449d0844ceee8fe511f0006425344e09b77
jylim26 <yejong98@gmail.com>
2025-09-20 23:05:43 +0900

    복습 [실버2/1541번/그리디] 잃어버린 괄호

---
+++ src/retry/problem_1541.java
@@ -0,0 +1,32 @@
+package retry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class problem_1541 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		String[] arr = line.split("-");
+
+		for (int i=0; i<arr.length; i++) {
+			if (arr[i].contains("+")) {
+
+				String[] plusArr = arr[i].split("\\+");
+				int sum = 0;
+				for (String s : plusArr) {
+					sum += Integer.parseInt(s);
+				}
+				arr[i] = String.valueOf(sum);
+			}
+		}
+
+		int result = Integer.parseInt(arr[0]);
+		for (int i=1; i<arr.length; i++) {
+			result -= Integer.parseInt(arr[i]);
+		}
+		System.out.println(result);
+	}
+}
+
